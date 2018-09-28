3f1f617373f979aef506237aed63fcc920a34b3c
Kemi Wang <kemi.wang@intel.com>
2018-09-28 15:07:22 +0800

    Spinlock: modify the generic version of spinlock to use mcs-style lock
    Todo: may we need a specific x86 version for that?
    
    Signed-off-by: Kemi Wang <kemi.wang@intel.com>

--- sysdeps/nptl/bits/pthreadtypes.h
+++ sysdeps/nptl/bits/pthreadtypes.h
@@ -100,7 +100,14 @@ typedef union
 
 #ifdef __USE_XOPEN2K
 /* POSIX spinlock data type.  */
-typedef volatile int pthread_spinlock_t;
+
+
+typedef struct
+{
+	volatile int spin_lock;
+	void *mcs_lock;
+	int token;
+} pthread_spinlock_t;
 
 
 /* POSIX barriers data type.  The structure of the type is
